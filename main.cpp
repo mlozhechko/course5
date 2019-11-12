6cfdab0bcaa92a4a47504ab60fb1f5f959840b03
Michael Lozhechko <mvlozhechko@gmail.com>
2019-11-13 02:06:32 +0300

    implemented new intersections find method

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <bitset>
+#include <algorithm>
 
 #include <vtkUnstructuredGridReader.h>
 #include <vtkUnstructuredGrid.h>
@@ -33,7 +35,7 @@ public:
     std::array<double, 4> get_boundaries() {
         /*
          * TODO:
-         * 1. rewrite with points instead of double
+         * 1. remove(!)
          */
         std::array<double, 4> res{};
         res[1] = res[0] = _points[0][0];
@@ -43,8 +45,6 @@ public:
             /*
              * GPU optimization to avoid if statements (blank)
              *
-             * TODO:
-             * check for accuracy losses
              */
             auto flag = static_cast<double>(res[0] < _points[i][0]);
             res[0] = flag * _points[i][0] + (1 - flag) * res[0];
@@ -88,28 +88,27 @@ std::ostream& operator<<(std::ostream& os, const lite_tetrahedron& lt)
 class line {
 public:
     explicit line(double x, double y) : _x(x), _y(y) {};
-
     double x() {
         return _x;
     }
     double y() {
         return _y;
     }
-    void add_possible_tetra_id(size_t id) {
-        _tetra_id.push_back(id);
+    void add_tetra_intersection(size_t id, const std::bitset<4>& polygon_intersections) {
+//        std::cout << "{" << x() << "," << y() << "}," << polygon_intersections << std::endl;
+        _tetra_intersections.emplace_back(id, polygon_intersections);
     }
 private:
     double _x, _y;
-    std::vector<size_t> _tetra_id;
+    std::vector<std::pair<size_t, std::bitset<4>>> _tetra_intersections;
 };
 
 class plane {
 public:
-    plane(size_t res_x, size_t res_y, const std::array<double, 4>& global_boundaries) {
+    plane(size_t res_x, size_t res_y, const std::array<double, 4>& global_boundaries)
+    : _global_boundaries(global_boundaries) {
         double delta_x(global_boundaries[0] - global_boundaries[1]);
         double delta_y(global_boundaries[2] - global_boundaries[3]);
-
-        _global_boundaries = global_boundaries;
 
         _step_x = delta_x / (res_x - 1.);
         _step_y = delta_y / (res_y - 1.);
@@ -128,44 +127,121 @@ public:
     }
 
     /*
-     * return amount of lines assumed to intersect tetrahedron
+     * return amount of lines to intersect polygons
      */
-    size_t add_possible_tetra_from_boundaries(const std::array<double, 4>& boundaries, size_t id) {
+    size_t find_intersections_with_tetrahedron(const lite_tetrahedron& tetra, size_t id) {
         /*
-         * TODO:
-         * 1. rewrite method to add all explicit intersection tetras from boundaries
+         * 0001 0, 1, 2 points
+         * 0010 0, 1, 3 points
+         * 0100 0, 2, 3 points
+         * 1000 1, 2, 3 points
          */
 
-        /*
-         * x_start = (x_min - global_x_min) / step
-         * */
-        size_t x_start = ceil((boundaries[1] - _global_boundaries[1]) / _step_x);
+        size_t sum(0);
+        sum += find_intersections_with_polygon({tetra[0], tetra[1], tetra[2]}, id, 0b0001);
+        sum += find_intersections_with_polygon({tetra[0], tetra[1], tetra[3]}, id, 0b0010);
+        sum += find_intersections_with_polygon({tetra[0], tetra[2], tetra[3]}, id, 0b0100);
+        sum += find_intersections_with_polygon({tetra[1], tetra[2], tetra[3]}, id, 0b1000);
 
-        /*
-         * x_end = (x_max - global_x_min) / step
-         * */
-        size_t x_end = ceil((boundaries[0] - _global_boundaries[1]) / _step_x);
-
-        /*
-         * y_start = (y_min - global_y_min) / step
-         */
-        size_t y_start = ceil((boundaries[3] - _global_boundaries[3]) / _step_y);
-
-        /*
-         * y_start = (y_min - global_y_min) / step
-         */
-        size_t y_end = ceil((boundaries[2] - _global_boundaries[3]) / _step_y);
-
-        for (size_t i = x_start; i < x_end; i++) {
-            for (size_t j = y_start; j < y_end; j++) {
-                _lines.at(i).at(j).add_possible_tetra_id(id);
-            }
+        if (sum % 2 == 1) {
+            throw std::runtime_error("critical error. odd number of intersections");
         }
 
-        return (x_end - x_start) * (y_end - y_start);
+        return sum / 2;
     }
 
 private:
+    /*
+     * all line equations is on X,Y plane
+     */
+
+    static inline double line_common_eq(std::array<double, 3> p1, std::array<double, 3> p2, std::array<double, 3> pos) {
+         return (p2[1] - p1[1]) * pos[0] + (p1[0] - p2[0]) * pos[1] + (p2[0] * p1[1] - p1[0] * p2[1]);
+    }
+
+    static inline double line_rev_function_eq(std::array<double, 3> p1, std::array<double, 3> p2, double y) {
+        return (p1[0] - p2[0]) * (y - p1[1]) / (p1[1] - p2[1]) + p1[0];
+    }
+
+    size_t find_intersections_with_polygon(std::array<std::array<double, 3>, 3> points, size_t id, std::bitset<4> polygon) {
+        size_t counter(0);
+
+        std::sort(points.begin(), points.end(), [](auto a, auto b){
+            return a[1] > b[1];
+        });
+
+        /*
+         * find p[1] pos relative to p[0] to p[2] line
+         */
+        double rel_qual = line_common_eq(points[0], points[2], points[1]);
+
+        /*
+         * let L be line passing through points p[0] and p[2]
+         *
+         * check if L is ascending and p[1] is below the line
+         */
+        bool asc_above = (points[0][0] >= points[2][0]) && (rel_qual >= 0);
+
+        /*
+         * check if L is descending and p[1] is above the line
+         */
+        bool des_below = (points[0][0] < points[2][0]) && (rel_qual > 0);
+
+        /*
+         * p[1] may on the left side or on right side of the line
+         * also p[1].y is greater than p[2].y and lower than p[0].y
+         *
+         * therefore we can fully describe position of triangle of plane, and use it
+         * to find grid area that intersects polygon.
+         *
+         * position is true if p[2] is on the right side of L
+         */
+        bool position = !(asc_above || des_below);
+
+        double y_max = points[0][1];
+        double y_min = points[2][1];
+
+        size_t y_max_index = std::floor((y_max - _global_boundaries[3]) / _step_y);
+        size_t y_min_index = std::ceil((y_min - _global_boundaries[3]) / _step_y);
+
+        size_t y_index_it = y_min_index;
+        double y_it = _lines[0][y_index_it].y();
+
+        /*
+         * iterate over triangle's edges and find intersection points
+         */
+        for (; y_index_it <= y_max_index; y_index_it++) {
+            double x_max(0), x_min(0);
+            if (position) {
+                x_min = line_rev_function_eq(points[0], points[2], y_it);
+                if (y_it < points[1][1]) {
+                    x_max = line_rev_function_eq(points[2], points[1], y_it);
+                } else {
+                    x_max = line_rev_function_eq(points[0], points[1], y_it);
+                }
+            } else {
+                x_max = line_rev_function_eq(points[0], points[2], y_it);
+                if (y_it < points[1][1]) {
+                    x_min = line_rev_function_eq(points[2], points[1], y_it);
+                } else {
+                    x_min = line_rev_function_eq(points[0], points[1], y_it);
+                }
+            }
+
+            size_t x_max_index = std::floor((x_max - _global_boundaries[1]) / _step_x);
+            size_t x_min_index = std::ceil((x_min - _global_boundaries[1]) / _step_x);
+
+            for (size_t i = x_min_index; i <= x_max_index; i++) {
+                _lines[i][y_index_it].add_tetra_intersection(id, polygon);
+                counter++;
+            }
+
+            y_it = y_it + _step_y;
+        }
+
+        return counter;
+    }
+
     /*
      * [] sequence is [x][y]
      */
@@ -230,15 +306,12 @@ int main() {
 
     std::cout << tetrahedron_vector[1000] << std::endl;
 
-    std::vector<std::array<double, 4>> tetrahedron_boundaries{};
-    tetrahedron_boundaries.reserve(tetrahedron_vector.size());
-
     /*
      * global domain boundaries
      * {x_max, x_min, y_max, y_min}
      */
     std::array<double, 4> global_boundaries{tetrahedron_vector.at(0).get_boundaries()};
-    std::transform(tetrahedron_vector.begin(), tetrahedron_vector.end(), std::back_inserter(tetrahedron_boundaries), [&global_boundaries](auto& x) {
+    std::for_each(tetrahedron_vector.begin(), tetrahedron_vector.end(), [&global_boundaries](auto& x) {
         /*
          * TODO:
          * check for bottleneck
@@ -253,13 +326,10 @@ int main() {
 
         global_boundaries[2] = std::max(global_boundaries[2], tmp[2]);
         global_boundaries[3] = std::min(global_boundaries[3], tmp[3]);
-        return tmp;
     });
 
-    std::cout << tetrahedron_boundaries[1000][3] << std::endl;
-
     std::cout
-        << "plane boundaries"
+        << "plane boundaries" << std::endl
         << "x_max = " << global_boundaries[0] << std::endl
         << "x_min = " << global_boundaries[1] << std::endl
         << "y_max = " << global_boundaries[2] << std::endl
@@ -269,21 +339,20 @@ int main() {
      * TODO
      * create program arguments for resolution
      */
-    size_t grid_resolution_x(620), grid_resolution_y(480);
+    size_t grid_resolution_x(200), grid_resolution_y(200);
     plane current_plane(grid_resolution_x, grid_resolution_y, global_boundaries);
 
     size_t min_intersections(0), max_intersections(0);
-    min_intersections = max_intersections = current_plane.add_possible_tetra_from_boundaries(tetrahedron_boundaries[0], 0);
+    min_intersections = max_intersections = current_plane.find_intersections_with_tetrahedron(tetrahedron_vector[0], 0);
 
-    for (size_t i = 1; i < tetrahedron_boundaries.size(); i++) {
-        size_t res = current_plane.add_possible_tetra_from_boundaries(tetrahedron_boundaries[i], i);
+    for (size_t i = 1; i < tetrahedron_vector.size(); i++) {
+        size_t res = current_plane.find_intersections_with_tetrahedron(tetrahedron_vector[i], i);
         min_intersections = std::min(res, min_intersections);
         max_intersections = std::max(res, max_intersections);
     }
 
-    std::cout
-        << "max possible intersections = " << max_intersections << std::endl
-        << "min possible intersections = " << min_intersections << std::endl;
+    std::cout << max_intersections << std::endl;
+    std::cout << min_intersections << std::endl;
 
     return 0;
 }
